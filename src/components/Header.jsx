1441c4d238ef6b24a5f310294d6f50d2baeb0ac3
Brayden <brsimoneau21@gmail.com>
2026-05-06 02:18:30 -0400

    Created styled header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import {FileDiff, Plus} from 'lucide-react'
+import ActionButton from './ActionButton.jsx'
+
+function Header() {
+    return (
+        <header className="bg-bg-surface border-b border-border py-4 px-8 flex flex-row justify-between items-center">
+            <div className="flex flex-row items-center gap-4">
+                <FileDiff className="bg-accent rounded-md p-1"></FileDiff>
+                <h1 className="font-bold text-2xl">Diffract</h1>
+            </div>
+
+            <div className="flex flex-row gap-4">
+                <ActionButton text="New" icon={<Plus size={15}></Plus>}></ActionButton>
+            </div>
+        </header>
+        
+    )
+}
+
+export default Header
