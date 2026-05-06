1359c4c680974d9fb8182d8fd8997e6617e6bfb0
Brayden <brsimoneau21@gmail.com>
2026-05-06 01:28:12 -0400

    Initialized app with Vite/React/TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet"></link>
+
     <App />
   </StrictMode>,
 )
