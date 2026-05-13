17b6aa001cb969e5917379faa6bc4ab31fe8662b
Brayden <brsimoneau21@gmail.com>
2026-05-13 12:41:17 -0700

    Create ResponseStatus component for footer

---
+++ src/components/ResponseStatus.jsx
@@ -0,0 +1,13 @@
+﻿function ResponseStatus({id, response}) {
+    let statusIcon = <span className="bg-diff-added rounded-full size-2.5 inline-block"></span>
+
+    if (!response || response.status != 200) {
+        statusIcon = <span className="bg-diff-removed rounded-full size-2.5 inline-block"></span>
+    }
+
+    return (
+        <p className="inline">{statusIcon} {id}: 200 OK • 100ms</p>
+    )
+}
+
+export default ResponseStatus;
