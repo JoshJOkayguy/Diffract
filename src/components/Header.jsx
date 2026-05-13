a4b234ef444053fce70251b8f6e15d222c839cc5
Brayden <brsimoneau21@gmail.com>
2026-05-13 14:38:01 -0700

    Updated logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import {FileDiff, Plus} from 'lucide-react'
+import {Plus} from 'lucide-react'
 import ActionButton from './ActionButton.jsx'
 
 function Header() {
     return (
         <header className="bg-bg-surface border-b border-border py-4 px-8 flex flex-row justify-between items-center">
             <div className="flex flex-row items-center gap-4">
-                <FileDiff aria-hidden="true" className="bg-accent rounded-md p-1"></FileDiff>
+                <img src="/public/diffract-logo.svg" alt="Diffract Logo" className="w-8 h-8"/>
                 <h1 className="font-bold text-2xl">Diffract</h1>
             </div>
 
