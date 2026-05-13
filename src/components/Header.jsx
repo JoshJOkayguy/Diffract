4d5302c6dd44e990e7c5034e82e2b242c215bf5a
Brayden <brsimoneau21@gmail.com>
2026-05-13 15:31:56 -0700

    Serve logo from root instead of /public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <header className="bg-bg-surface border-b border-border py-4 px-8 flex flex-row justify-between items-center">
             <div className="flex flex-row items-center gap-4">
-                <img src="/public/diffract-logo.svg" alt="Diffract Logo" className="w-8 h-8"/>
+                <img src="/diffract-logo.svg" alt="Diffract Logo" className="w-8 h-8"/>
                 <h1 className="font-bold text-2xl">Diffract</h1>
             </div>
 
